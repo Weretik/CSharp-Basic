6623ac2d203622467e5509cc434911366b155088
Weretik <vitaliythupin@gmail.com>
2025-02-20 21:36:49 +0200

    Implemented solution for Task 3 on Classes and Objects - Class Diagrams.

--- 2. Classes and objects. Class diagrams/Program.cs
+++ 2. Classes and objects. Class diagrams/Program.cs
@@ -26,6 +26,95 @@ Console.WriteLine($"{amountUAH} UAH = {convertedAmount} {currency}");
 double amountBackInUAH = converter.ConvertToUah(currency, convertedAmount);
 Console.WriteLine($"{convertedAmount} {currency} = {amountBackInUAH} UAH");
 
+
+/*
+ * Завдання 3 
+
+Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Потрібно створити клас Employee. 
+У тілі класу створити користувальницький конструктор, який приймає два рядкові аргументи, та ініціалізує поля, 
+що відповідають прізвищу та імені співробітника. Створити метод, що розраховує оклад співробітника (залежно від посади та стажу) 
+та податковий збір. Написати програму, яка виводить на екран інформацію про співробітника (прізвище, ім'я, посада), оклад та податковий збір. 
+*/
+Console.WriteLine();
+Console.WriteLine("Введіть ім'я співробітника:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Введіть прізвище співробітника:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Введіть посаду співробітника (manager, accountant, supervisor):");
+string position = Console.ReadLine();
+
+Console.WriteLine("Введіть кількість років стажу:");
+int yearsExperience = Convert.ToInt32(Console.ReadLine());
+
+Employee employee = new Employee(firstName, lastName, position, yearsExperience);
+employee.DisplayInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Завдання 3
+class Employee(string firstName, string lastName, string position, int yearsExperience)
+{
+    public string? FirstName { get; set; } = firstName;
+    public string? LastName { get; set; } = lastName;
+    public string? Position { get; set; } = position;
+    public int YearsExperience { get; set; } = yearsExperience;
+
+
+    public Employee(string firstName, string lastName) : this(firstName, lastName, "manager", 0)
+    {
+    }
+    public double CalculateSalary()
+    {
+        double baseSalary = 20_000; 
+        double experienceBonus = YearsExperience * 500;
+
+        double salary = Position switch
+        {
+            "manager" => baseSalary + experienceBonus + 1000,
+            "accountant" => baseSalary + experienceBonus + 3000,
+            "supervisor" => baseSalary + experienceBonus + 5000,
+            _ => baseSalary + experienceBonus
+        };
+
+        return salary;
+    }
+    public double CalculateTax(double salary)
+    {
+        return salary * 0.22; 
+    }
+    public void DisplayInfo()
+    {
+        double salary = CalculateSalary();
+        double tax = CalculateTax(salary);
+
+        Console.WriteLine("Інформація про співробітника:");
+        Console.WriteLine($"Ім'я: {FirstName}");
+        Console.WriteLine($"Прізвище: {LastName}");
+        Console.WriteLine($"Посада: {Position}");
+        Console.WriteLine($"Оклад: {salary} грн");
+        Console.WriteLine($"Податковий збір (22%): {tax} грн");
+    }
+
+}
+
+
 // Завдання 2 
 class Converter
 {
