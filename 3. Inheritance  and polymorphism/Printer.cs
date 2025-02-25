9fe6ab12bb52ac806f181aa92faa4c5ba3e5a794
Weretik <vitaliythupin@gmail.com>
2025-02-25 21:59:58 +0200

    Completed Task 5 - Implemented required functionality

---
+++ 3. Inheritance  and polymorphism/Printer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.ResetColor();
+            Console.WriteLine(value);
+        }
+    }
+
+    class RedPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+
+    class GreenPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+
+    class BluePrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
--- 3. Inheritance  and polymorphism/Program.cs
+++ 3. Inheritance  and polymorphism/Program.cs
@@ -2,6 +2,7 @@
 using Task2;
 using Task3;
 using Task4;
+using Task5;
 
 Console.InputEncoding = System.Text.Encoding.Unicode;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
@@ -87,3 +88,25 @@ worker.OpenDocument();
 worker.EditDocument();
 worker.SaveDocument();
 
+/*
+ * Завдання 5
+
+Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+Потрібно: Створити клас Printer. У тілі класу створіть метод void Print(string value), 
+що виводить на екран значення аргументу. Реалізуйте можливість того, щоб у разі успадкування від даного класу інших класів, 
+та виклику відповідного методу їх екземпляра, рядки, передані як аргументи методів, виводилися різними кольорами. 
+Обов'язково використовуйте типи.
+*/
+Console.WriteLine();
+Printer[] printers = {
+            new Printer(),       
+            new RedPrinter(),    
+            new GreenPrinter(),
+            new BluePrinter() 
+        };
+
+// Викликаємо метод Print() для кожного об'єкта
+foreach (var printer in printers)
+{
+    printer.Print("Це текст, виведений у класі " + printer.GetType().Name);
+}
