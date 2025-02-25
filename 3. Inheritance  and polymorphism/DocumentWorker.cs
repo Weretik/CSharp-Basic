b5a2151d98d38fb8150f2422ca4701eee9327ae7
Weretik <vitaliythupin@gmail.com>
2025-02-25 21:29:13 +0200

    Completed Task 4 - Implemented required functionality

---
+++ 3. Inheritance  and polymorphism/DocumentWorker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ відкритий.");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редагування документа доступне у версії PRO.");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Збереження документа доступне у версії PRO.");
+        }
+    }
+
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ відредаговано.");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережено у старому форматі, збереження в інших форматах є у версії EXPERT.");
+        }
+    }
+
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережений у новому форматі.");
+        }
+    }
+}
--- 3. Inheritance  and polymorphism/Program.cs
+++ 3. Inheritance  and polymorphism/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Task2;
 using Task3;
+using Task4;
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
 /*
  * Завдання 2 
 
@@ -35,6 +39,51 @@ Plane plane = new Plane(100, 200, 500000, 900, 2020, 10000, 150);
 Car car = new Car(50, 75, 30000, 180, 2018);
 Ship ship = new Ship(300, 400, 1000000, 60, 2015, 500, "Одеса");
 
+plane.ShowInfo();
 car.ShowInfo();
 ship.ShowInfo();
+
+
+/* 
+ * Завдання 4 
+
+Використовуючи Visual Studio, створіть проект за шаблоном Console Application, потрібно створити клас DocumentWorker. 
+У тілі класу створіть три методи OpenDocument(), EditDocument(), SaveDocument(). У тіло кожного з методів додайте виведення
+на екран відповідних рядків: "Документ відкритий", "Редагування документа у версії Про", "Збереження документа у версії Про".
+
+Створіть похідний клас ProDocumentWorker. Перевизначте відповідні методи, при перевизначенні методів виводьте наступні рядки:
+"Документ відредаговано", "Документ збережено у старому форматі, збереження в інших форматах є у версії Експерт".
+Створіть похідний клас ExpertDocumentWorker від базового класу ProDocumentWorker. Перевизначте відповідний спосіб. 
+При викликі даного методу необхідно виводити на екран "Документ збережений у новому форматі".
+У тілі методу Main() реалізуйте можливість прийому від користувача номер ключа доступу pro і exp. Якщо користувач 
+не вводить ключ, він може користуватися лише безкоштовною версією (створюється екземпляр базового класу), 
+якщо користувач ввів номери ключа доступу pro і exp, то повинен створити екземпляр відповідної версії класу, 
+наведений до базового - DocumentWorker.
+*/
+
+Console.WriteLine("\nВведіть ключ доступу (pro / exp):");
+string key = Console.ReadLine();
+
+DocumentWorker worker;
+
+
+if (key == "pro")
+{
+    worker = new ProDocumentWorker();
+    Console.WriteLine("Активована версія PRO.");
+}
+else if (key == "exp")
+{
+    worker = new ExpertDocumentWorker();
+    Console.WriteLine("Активована версія EXPERT.");
+}
+else
+{
+    worker = new DocumentWorker();
+    Console.WriteLine("Використовується безкоштовна версія.");
+}
+
+worker.OpenDocument();
+worker.EditDocument();
+worker.SaveDocument();
 
