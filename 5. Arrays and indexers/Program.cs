d291c385cdc8ad9432e87f9596f9b68a45f5b62e
Weretik <vitaliythupin@gmail.com>
2025-02-26 21:01:43 +0200

    Completed Task 2. Creating an array of N elements

--- 5. Arrays and indexers/Program.cs
+++ 5. Arrays and indexers/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+/*
+ * Завдання 2
+Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+
+Потрібно: 
+Створити масив розмірністю N елементів, заповнити його довільними цілими значеннями. 
+Вивести найбільше значення масиву, найменше значення масиву, загальну суму елементів, 
+середнє арифметичне всіх елементів, вивести всі непарні значення.
+*/
+
+Random random = new();
+
+Console.WriteLine("Введіть розмірність масиву");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 20);
+}
+
+
+Console.WriteLine($"Найбільше значення масиву: {array.Max()}");
+
+Console.WriteLine($"Найменше значення масиву: {array.Min()}");
+
+Console.WriteLine($"Загальну суму елементів: {array.Sum()}");
+
+Console.WriteLine($"Середнє арифметичне всіх елементів: {array.Average()}");
+
+Console.WriteLine("Всі непарні значення:");
+foreach (var num in array)
+{
+    if (num % 2 != 0)
+    {
+        Console.WriteLine(num);
+    }
+}
