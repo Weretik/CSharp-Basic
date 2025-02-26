eec747ebee5e56a749823f0855b723818b169cb6
Weretik <vitaliythupin@gmail.com>
2025-02-26 14:53:59 +0200

    Completed Task 3. AbstractHandler class is created

---
+++ 4. Abstraction. Abstract classes and interfaces/AbstractHandler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+    class XMLHandler : AbstractHandler
+    {
+        public override void Change()
+        {
+            Console.WriteLine("XML документ було змінено");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("XML документ було створено");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("XML документ було відкрито");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("XML документ було збережено");
+        }
+    }
+    class TXTHandler : AbstractHandler
+    {
+        public override void Change()
+        {
+            Console.WriteLine("TXT документ було змінено");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("TXT документ було створено");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("TXT документ було відкрито");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("TXT документ було збережено");
+        }
+    }
+    class DOCHandler : AbstractHandler
+    {
+        public override void Change()
+        {
+            Console.WriteLine("DOC документ було змінено");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("DOC документ було створено");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("DOC документ було відкрито");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("DOC документ було збережено");
+        }
+    }
+}
+
+/*
+ * Створити клас AbstractHandler. У тілі класу створити методи void Open(), void Create(), void Change(), void Save(). 
+Створити похідні класи XMLHandler, TXTHandler, DOCHandler від базового класу AbstractHandler. Написати програму, 
+яка виконуватиме визначення документа і для кожного формату мають бути методи відкриття, створення, редагування, 
+збереження певного формату документа.
+*/
--- 4. Abstraction. Abstract classes and interfaces/Program.cs
+++ 4. Abstraction. Abstract classes and interfaces/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Abstraction;
+using System;
+using System.Diagnostics;
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+/*
+ * Завдання 2 
+
+Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+
+Потрібно: 
+
+Створити клас AbstractHandler. У тілі класу створити методи void Open(), void Create(), void Change(), void Save(). 
+Створити похідні класи XMLHandler, TXTHandler, DOCHandler від базового класу AbstractHandler. Написати програму, 
+яка виконуватиме визначення документа і для кожного формату мають бути методи відкриття, створення, редагування, 
+збереження певного формату документа.
+*/
+
+Console.WriteLine("Виберіть формат документа (XML / DOC / TXT)");
+string? formatDocument = Console.ReadLine();
+
+AbstractHandler documentHandler;
+
+switch (formatDocument)
+{
+	case "XML":
+        documentHandler = new XMLHandler();
+        break;
+    case "DOC":
+        documentHandler = new DOCHandler();
+        break;
+    default:
+        documentHandler = new TXTHandler();
+        break;
+}
+
+documentHandler.Create();
+documentHandler.Open();
+documentHandler.Change();
+documentHandler.Save();
