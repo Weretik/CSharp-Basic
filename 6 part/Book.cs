3fff3795f1c31591d9d53a4936bcc0233399f10a
Weretik <vitaliythupin@gmail.com>
2025-02-28 23:05:48 +0200

    Created project: Static and Nested Classes. Completed Task 2. Created static class FindAndReplaceManager.

---
+++ 6 part/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._Static_and_nested_classes
+{
+    class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Пошук рядка : " + str);
+        }
+    }
+
+    static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            Console.WriteLine("Пошук рядка : " + str);
+        }
+    }
+}
---
+++ 6 part/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _6._Static_and_nested_classes
+
+{
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            /*
+             * Завдання 2 
+             * Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+             * Потрібно: Створити статичний клас FindAndReplaceManager з методом void FindNext(string str) для пошуку за книгою з прикладу уроку 005_Book. 
+             * При виклику цього методу проводиться послідовний пошук рядка в книзі.
+             */
+
+            FindAndReplaceManager.FindNext("Відразу після пробудження героїня потрапляє під загрозу");
+        }
+    }
+}
