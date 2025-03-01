095d244b5402bf91f75f001e4b88a5bb721b1cc8
Weretik <vitaliythupin@gmail.com>
2025-03-01 18:24:05 +0200

    Created project: Structures and their types

---
+++ 7. Structures and their types/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _7._Structures_and_their_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
