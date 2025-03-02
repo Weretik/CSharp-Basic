0b4414f76ad5a7a65366f403da07974b1ef83df4
Weretik <vitaliythupin@gmail.com>
2025-03-02 11:58:56 +0200

    Created project: Enumeration.

---
+++ 8. Enumeration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8._Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
