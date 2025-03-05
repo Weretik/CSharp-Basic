e33b51e660323b1c7b070b67e0c69157d102a3cf
Weretik <vitaliythupin@gmail.com>
2025-03-05 22:01:07 +0200

    Completed task 3: Created Dictionary class.

---
+++ 11. Generic Constraints/Dictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Generic_Constraints
+{
+    public class Dictionary<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+        private int count;
+
+        public Dictionary()
+        {
+            keys = new TKey[10]; 
+            values = new TValue[10];
+            count = 0;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (count < keys.Length)
+            {
+                keys[count] = key;
+                values[count] = value;
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Немає місця для додавання нових елементів");
+            }
+        }
+        public TValue this[TKey key]
+        {
+            get
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (EqualityComparer<TKey>.Default.Equals(keys[i], key))
+                    {
+                        return values[i];
+                    }
+                }
+                return default;
+            }
+        }
+        public int Count => count;
+    }
+}
--- 11. Generic Constraints/Program.cs
+++ 11. Generic Constraints/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
             /*
              * Завдання 2
 
@@ -23,6 +25,31 @@
             Console.WriteLine($"В автопарку {carCollection.LenthCarColection} машин");
             carCollection.ClearAllCar();
             Console.WriteLine($"В автопарку {carCollection.LenthCarColection} машин");
+
+
+            /*
+             * 
+             * Завдання 3
+
+            Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+            Створіть клас Dictionary. Реалізуйте у найпростішому наближенні можливість використання його екземпляра аналогічно екземпляру класу 
+            Dictionary із простору імен System.Collections.Generic. Мінімально необхідний інтерфейс взаємодії з екземпляром повинен включати метод додавання пар елементів, 
+            індексатор для отримання значення елемента за вказаним індексом і властивість тільки для читання для отримання загальної кількості пар елементів.
+            */
+            Console.WriteLine();
+            var dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("apple", 10);
+            dictionary.Add("banana", 20);
+
+            Console.WriteLine($"Кількість яблук: {dictionary["apple"]}");
+            Console.WriteLine($"Кількість бананів: {dictionary["banana"]}");
+
+            Console.WriteLine($"Кількість пар елементів: {dictionary.Count}");
         }
+
+
+
     }
+    
 }
