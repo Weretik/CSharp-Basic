757cc3fa26f87bce856c8ec8f3239a2a03753088
Weretik <vitaliythupin@gmail.com>
2025-03-05 19:49:27 +0200

    Created project: Generic. Completed Task 2. Created MyList class with Add method, indexer, and read-only property for element count.

---
+++ 10. Generic/MyList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._Generic
+{
+    
+    class MyList<T>
+    {
+        private T[] array;
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+
+        public void AddToList(T value)
+        {
+            if (array == null)
+            {
+                array = new T[] { value };
+            }
+            else
+            {
+                T[] newArray = new T[array.Length + 1];
+                array.CopyTo(newArray, 0);
+                newArray[array.Length] = value;
+                array = newArray;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return array == null ? 0 : array.Length;
+            }
+        }
+    }
+}
---
+++ 10. Generic/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _10._Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            /*
+             * Завдання 2 
+
+            Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Створіть клас MyList. 
+            Реалізуйте у найпростішому наближенні можливість використання його екземпляра аналогічно екземпляру класу List. 
+            Мінімально необхідний інтерфейс взаємодії з екземпляром повинен включати метод додавання елемента, індексатор для 
+            отримання значення елемента за вказаним індексом і властивість тільки для читання для отримання загальної кількості елементів.
+            */
+
+            MyList<int> list = new MyList<int>();
+            list.AddToList(25);
+            list.AddToList(35);
+            list.AddToList(45);
+            list.AddToList(55);
+
+            Console.WriteLine($"Другий елемент в списку: {list[1]}");
+            Console.WriteLine($"Загальна кількість елементыв в списку: {list.Count}");
+        }
+    }
+}
