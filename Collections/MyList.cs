f7a58620ae147c00a684a29c45ada397a13574f4
Weretik <vitaliythupin@gmail.com>
2025-03-08 18:35:41 +0200

    Created project Collections. Completed Task 2. Implemented MyList with Add, indexer, element count, and iteration.

---
+++ Collections/MyList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class MyList <T>: IEnumerable<T>
+    {
+        T[] array;
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (array == null)
+            {
+                array = new T[1];
+                array[0] = item;
+            }
+            else
+            {
+                T[] tempArray = new T[array.Length + 1];
+                array.CopyTo(tempArray, 0);
+                tempArray[^1] = item;
+                array = tempArray;
+            }
+        }
+
+        public int Lenth
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in array)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Завдання 2
+
+                Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Створіть колекцію MyList. 
+                Реалізуйте у найпростішому наближенні можливість використання її екземпляра аналогічно екземпляру класу List. 
+                Мінімально необхідний інтерфейс взаємодії з екземпляром повинен включати метод додавання елемента,
+                індексатор для отримання значення елемента за вказаним індексом і властивість тільки для читання для отримання загальної кількості елементів. 
+                Реалізуйте можливість перебору елементів колекції у циклі наперед.
+            */
+
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(12);
+            myList.Add(6);
+            myList.Add(25);
+
+            Console.WriteLine($"Довжина колекції: { myList.Lenth}");
+            Console.WriteLine($"Перший елемент масиву: {myList[0]}");
+            Console.WriteLine("Весь масив:");
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
