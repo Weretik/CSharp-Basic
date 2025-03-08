326ab2fd657600168fa02bd89e409a2e8901d3bb
Weretik <vitaliythupin@gmail.com>
2025-03-08 18:42:57 +0200

    Completed Task 3. Created MyDictionary collection with Add method, indexer, element count property, and forward iteration.

---
+++ Collections/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+        private int count;
+
+        public MyDictionary()
+        {
+            keys = new TKey[10];
+            values = new TValue[10];
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (count == keys.Length)
+            {
+                Array.Resize(ref keys, keys.Length * 2);
+                Array.Resize(ref values, values.Length * 2);
+            }
+
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        }
+
+        public TValue this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                    throw new Exception("Індекс поза межами колекції.");
+                return values[index];
+            }
+        }
+
+        public void Iterate()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Ключ: {keys[i]}, Значення: {values[i]}");
+            }
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Collections
+﻿using System.Text;
+
+namespace Collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
             /*
              * Завдання 2
 
@@ -27,6 +32,29 @@
             {
                 Console.WriteLine(item);
             }
+
+            /*
+             * Завдання 3 
+
+            Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+            Створіть колекцію MyDictionary. Реалізуйте у найпростішому наближенні можливість використання її екземпляра аналогічно екземпляру класу Dictionary. 
+            Мінімально необхідний інтерфейс взаємодії з екземпляром повинен включати метод додавання елемента, 
+            індексатор для отримання значення елемента за вказаним індексом і властивість тільки для читання для отримання загальної кількості елементів. 
+            Реалізуйте можливість перебору елементів колекції у циклі наперед.
+            */
+
+            MyDictionary<string, int> myDict = new MyDictionary<string, int>();
+
+            myDict.Add("Молоко", 5);
+            myDict.Add("Сахар", 10);
+            myDict.Add("Йогурт", 15);
+
+            Console.WriteLine("Кількість елементів: " + myDict.Count);
+
+            Console.WriteLine("1-й елемент за індексом: " + myDict[0]);
+
+            Console.WriteLine("\nПеребір елементів:");
+            myDict.Iterate();
         }
     }
 }
