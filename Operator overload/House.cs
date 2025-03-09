47881d75de9a5a50b81860ba7d0ba90c94f6014c
Weretik <vitaliythupin@gmail.com>
2025-03-09 14:17:27 +0200

    Completed Task 3. Created House class with fields and properties. Implemented Clone and DeepClone methods with basic checking.

---
+++ Operator overload/House.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overload
+{
+    
+    class House
+    {
+        public int Area { get; set; }
+        public string Address { get; set; }
+        public House(int area, string address)
+        {
+            Area = area;
+            Address = address;
+        }
+        public House Clone()
+        {
+            return (House)this.MemberwiseClone();
+        }
+        public House DeepClone()
+        {
+            return new House(Area, Address);
+        }
+
+        public override string ToString()
+        {
+            return $"Площа = {Area}, Адреса = {Address}";
+        }
+    }
+}
--- Operator overload/Program.cs
+++ Operator overload/Program.cs
@@ -29,7 +29,34 @@ namespace Operator_overload
                 Console.WriteLine(block2);
             }
 
+            /*
+             * Завдання 3
 
+            Створіть клас House з двома полями та властивостями. 
+            Створіть два методи Clone() та DeepClone(), які виконують поверхневе та глибоке копіювання відповідно.
+            Реалізуйте найпростіший спосіб перевірки.
+            */
+            Console.WriteLine();
+            House house1 = new House(100, "вул. Лесі Українки, 1");
+            House house2 = house1.Clone();
+            House house3 = house1.DeepClone();
+
+            Console.WriteLine($"Дім 1:{house1}");
+            Console.WriteLine($"Дім 2:{house2}");
+            Console.WriteLine($"Дім 3:{house3}");
+            
+            house2.Area = 200;
+            house2.Address = "вул. Шевченка, 2";
+
+            house3.Area = 300;
+            house3.Address = "вул. Велика кільцева, 3";
+
+            Console.WriteLine();
+            Console.WriteLine("Після змін площи та адреси");
+
+            Console.WriteLine($"Дім 1:{house1}");
+            Console.WriteLine($"Дім 2:{house2}");
+            Console.WriteLine($"Дім 3:{house3}");
         }
     }
 }
