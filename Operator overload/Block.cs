f4a7c88fa82059262c2415e87dc3b1086ea6b4d4
Weretik <vitaliythupin@gmail.com>
2025-03-09 13:52:47 +0200

    Created project Collections. Completed Task 2. Created Block class with fields and overridden Equals and ToString methods.

---
+++ Operator overload/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overload
+{
+    
+    class Block
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public Block(int a, int b, int c, int d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Block block = (Block)obj;
+            return A == block.A && B == block.B && C == block.C && D == block.D;
+        }
+        public override string ToString()
+        {
+            return $"Block: A = {A}, B = {B}, C = {C}, D = {D}";
+        }
+    }
+}
---
+++ Operator overload/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Operator_overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            /*
+             * Завдання 2
+
+            Створіть клас Block із 4 полями сторін, перевизначте в ньому методи: 
+            Equals - здатний порівнювати блоки між собою, 
+            ToString - повертає інформацію про поля блоку.
+            */
+            Block block1 = new Block(1, 2, 3, 4);
+            Block block2 = new Block(1, 2, 3, 4);
+            if (block1.Equals(block2))
+            {
+                Console.WriteLine("Блоки однакові");
+                Console.WriteLine(block1);
+            }
+            else
+            {
+                Console.WriteLine("Блоки не однакові");
+                Console.WriteLine(block1);
+                Console.WriteLine(block2);
+            }
+
+
+        }
+    }
+}
