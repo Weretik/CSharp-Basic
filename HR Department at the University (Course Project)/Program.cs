f3d4cab840bedfd90396b7ab8d40e77775de52e0
Weretik <vitaliythupin@gmail.com>
2025-03-30 18:04:01 +0300

    feat(project): Initialize course project "HR Department at the University".

---
+++ HR Department at the University (Course Project)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HR_Department_at_the_University__Course_Project_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
