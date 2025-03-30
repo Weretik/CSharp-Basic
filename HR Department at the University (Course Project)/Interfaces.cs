176fe3ea426e2114ef1740009d32cd8fde8b9809
Weretik <vitaliythupin@gmail.com>
2025-03-30 18:50:58 +0300

    feat(project): Add abstract Person class and related interfaces for HR Department system.

---
+++ HR Department at the University (Course Project)/Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HR_Department_at_the_University__Course_Project_
+{
+    internal interface IStudent
+    {
+        Group Group { get; set; }
+        List<IParent> Parents { get; set; }
+    }
+    internal interface ITeacher
+    {
+        Department ProfileDepartment { get; set; }
+        string Position { get; set; }
+    }
+}
---
+++ HR Department at the University (Course Project)/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * и. Відомості про кожну людину мають
+містити прізвище, ім'я, по батькові, стать, паспортні дані, місце проживання. Для студентів додатково має бути
+інформація про батьків та групу. Для викладачів додатково має бути інформація про кафедру та посаду.
+Потрібно передбачити можливу ситуацію, коли одна й та сама людина може бути одночасно студентом,
+батьком і викладачем. Один з батьків може мати кілька дітей-студентів.
+*/
+
+namespace HR_Department_at_the_University__Course_Project_
+{
+    internal abstract class Person
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string Gender { get; set; }
+        public string PassportData { get; set; }
+        public string Address { get; set; }
+
+        public string FullName => $"{LastName} {FistName} {Patronymic}";
+
+
+    }
+}
